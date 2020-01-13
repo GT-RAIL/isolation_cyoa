b381ab8ca36c47cf195ea0b2d9e88a91884af8d9
Siddhartha Banerjee <banerjs.sid@gmail.com>
2020-01-13 10:48:10 -0500

    Small QoL to the script to start

--- docker/run_docker_interactive.sh
+++ docker/run_docker_interactive.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# Run the torch docker container
+# Run the website docker container. If there is already a container running,
+# then exec into the container
 
 set -e
 
@@ -7,9 +8,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Run the docker container
-echo "$@"
 cd ${DIR}/..
+
+
+# Create the container OR exec into it
 docker run -it --rm --privileged --name website --net=host \
     -v $(pwd):/home/banerjs/website \
     -v $HOME/Documents/GT/Research/Data/arbitration:/home/banerjs/data \
-    banerjs/fault-isolation:cyoa_website bash
+    banerjs/fault-isolation:cyoa_website bash \
+|| docker exec -it website bash
