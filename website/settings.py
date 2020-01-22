45a0b968a3df70f0036f31d7aa3fd66b236908c0
Siddhartha Banerjee <banerjs.sid@gmail.com>
2020-01-22 18:37:48 -0500

    Disable debug in prod

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY', 'come@me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv('ENVIRONMENT') == 'dev')
 
 ALLOWED_HOSTS = []
 
