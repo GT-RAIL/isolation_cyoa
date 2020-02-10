05d82c72589dab3303b415a8e44ec1496b469e80
Siddhartha Banerjee <banerjs.sid@gmail.com>
2020-02-10 17:45:28 -0500

    Preserve rudimentary FK relations

--- website/management/commands/dropbox_to_db.py
+++ website/management/commands/dropbox_to_db.py
@@ -6,6 +6,7 @@ import sys
 import json
 import pprint
 import traceback
+import collections
 
 import dropbox
 
@@ -13,6 +14,7 @@ from django.conf import settings
 from django.core import management
 from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 
 from dining_room.models import User, StudyManagement
 
@@ -51,21 +53,29 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         dbx_folder = os.path.join(settings.DROPBOX_ROOT_PATH, settings.DROPBOX_DATA_FOLDER, options['data_directory'])
 
-        # Create a dictionary of files to create (and associated settings)
+        # Create a dictionary of files to create (and associated settings). This
+        # is a tad bit more complicated as we can remap pk's from those in the
+        # stored fixture to those of the fixture data after it has been inserted
+        # into the DB
         fixtures_to_download = [
             (Command.MANAGEMENT_DETAILS_FILE, {
                 'name': 'dining_room.StudyManagement',
-                'model': StudyManagement
+                'model': StudyManagement,
             }),
             (Command.USER_DETAILS_FILE, {
                 'name': 'dining_room.User',
                 'model': User,
-                'ignore_fields': { 'groups', 'user_permissions', 'study_management' }
+                'ignore_fields': { 'groups', 'user_permissions', 'study_management' },
+
+                # A tuple of the field name to use and the fixture earlier in
+                # the download process to link to
+                'link_to': ('study_management', Command.MANAGEMENT_DETAILS_FILE),
             }),
         ]
 
         # Loop through the fixtures to dump, and the settings to dump and
         # synchronize them with dropbox
+        fixture_pk_mappings = collections.defaultdict(dict)
         for filename, model_details in fixtures_to_download:
             # The user data is dumped and synchronized
             if options.get('verbosity') > 0:
@@ -89,10 +99,20 @@ class Command(BaseCommand):
                     data_dict = { k: v for k, v in row['fields'].items() if k not in model_details.get('ignore_fields', {}) }
                     item = model_details['model'](**data_dict)
                     item.save()
-                    continue
-                except Exception as e:
+
+                    # Save the mapping
+                    fixture_pk_mappings[filename][row['pk']] = item.pk
+
+                    # If we don't need to link the data, then just continue
+                    if model_details.get('link_to') is None:
+                        continue
+
+                except IntegrityError as e:
+                    item = None
+
                     if not options.get('ignore_duplicates') and not options.get('confirm_duplicates') and not options.get('update_duplicates'):
                         raise CommandError(f"Failure saving model: {e}\n{traceback.format_exc()}")
+
                     elif not options.get('update_duplicates'):
                         self.stdout.write(f"Not writing row for model {row['model']} with pk {row['pk']}: {e}")
                         if options.get('confirm_duplicates'):
@@ -106,24 +126,39 @@ class Command(BaseCommand):
                             elif response.lower() == 'i':
                                 continue
 
-                # If we're here, then update the data
-                queryset = model_details['model'].objects.all()
-                for field, value in data_dict.items():
-                    new_queryset = queryset.filter(**{field: value})
-                    if new_queryset.count() == 0:
-                        continue
+                # If we're here, then we need to update the data if it doesn't
+                # already have a pk
+                if item is None:
+                    queryset = model_details['model'].objects.all()
+                    for field, value in data_dict.items():
+                        new_queryset = queryset.filter(**{field: value})
+                        if new_queryset.count() == 0:
+                            continue
 
-                    queryset = new_queryset
-                    if queryset.count() == 1:
-                        break
+                        queryset = new_queryset
+                        if queryset.count() == 1:
+                            break
 
-                if queryset.count() != 1:
-                    raise CommandError("Could not find a suitable item to update in DB")
+                    if queryset.count() != 1:
+                        raise CommandError("Could not find a suitable item to update in DB")
 
-                item = queryset[0]
-                for field, value in data_dict.items():
-                    setattr(item, field, value)
-                item.save()
+                    item = queryset[0]
+                    for field, value in data_dict.items():
+                        setattr(item, field, value)
+                    item.save()
+
+                # Update the linkages on the item
+                if model_details.get('link_to') is not None:
+                    field, fixture_filename = model_details['link_to']
+
+                    # Set the raw ID of the linked model
+                    setattr(
+                        item,
+                        f"{field}_id",
+                        fixture_pk_mappings[fixture_filename][row['fields'][field]]
+                    )
+
+                    item.save()
 
             # Print complete
             if options.get('verbosity') > 0:
