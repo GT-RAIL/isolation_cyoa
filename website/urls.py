93231dbf84f3e1f86c1b8f5515372856361f88bf
Siddhartha Banerjee <banerjs.sid@gmail.com>
2020-03-26 21:41:18 -0400

    Add a new url for the ROMAN results

--- website/urls.py
+++ website/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('dining_room.urls')),
     path('admin/', admin.site.urls),
+    path('roman2020_results', TemplateView.as_view(template_name='website/roman2020.html')),
 ]
