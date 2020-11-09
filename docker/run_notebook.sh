335b9de7921f82a3f365c38b860f162913986d56
Siddhartha Banerjee <banerjs.sid@gmail.com>
2020-11-09 17:09:27 -0500

    Small updates

--- docker/run_notebook.sh
+++ docker/run_notebook.sh
@@ -12,12 +12,12 @@ cd ${DIR}/..
 
 
 # Create the container OR exec into it
-docker run -d --rm --runtime=nvidia --privileged --name analysis --net=host \
+docker run -d --rm --gpus all --privileged --name isolation_cyoa --net=host \
     -v $(pwd):/home/banerjs/website \
     -v $CODEBASE_DIR/isolation/models:/notebooks \
     -v $DATABASE_DIR/arbitration:/home/banerjs/data \
-    banerjs/fault-isolation:cyoa_analysis \
-|| docker exec -it analysis "$@"
+    banerjs/fault-isolation:cyoa_analysis jupyter notebook --ip=0.0.0.0 --port=9000 \
+|| docker exec -it isolation_cyoa "$@"
 
 # Recommended command to run visdom
-# docker exec -d analysis /bin/bash -c 'source /venv_entrypoint.sh && python -m visdom.server -port 8080'
+# docker exec -d isolation_cyoa /bin/bash -c 'source /venv_entrypoint.sh && python -m visdom.server -port 8080'
